5dc752cd06dc9e3084ab4699efa451c2ee5de71d
jerryntom <jaroslawtomczuk.ofi@gmail.com>
2022-11-06 18:42:17 +0100

    02 Zmieniona funkcja Policz

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
+int Policz(int a, int b) {
+    return a*b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "Wynik:" << Policz(5, 6) << std::endl;
     return 0;
 }
